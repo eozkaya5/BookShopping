4e42b13ee9d15a3f7c7d85970b1327cf018aafc6
eozkaya5 <eozkaya675@gmail.com>
2020-11-05 13:47:40 +0300

    Veritabanına alışveriş için model eklendi.

--- BookShopping/Models/ShoppingModel/Payment.cs
+++ BookShopping/Models/ShoppingModel/Payment.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BookShopping.Models.ShoppingModel
+namespace BookShopping.Models.ShoppingModel 
 {
     public class Payment
     {
