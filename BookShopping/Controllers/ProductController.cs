4f3ca026b2cf1aadebdd7af337723ea23c971f9a
eozkaya5 <eozkaya675@gmail.com>
2020-11-11 03:20:56 +0300

    Product sayfası tasarımı oluşturuldu. Ekle, sil, düzenle işlemleri eklendi.

--- BookShopping/Controllers/ProductController.cs
+++ BookShopping/Controllers/ProductController.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using BookShopping.Models.Context;
 using BookShopping.Models.ShoppingModel;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace BookShopping.Controllers
 {
@@ -18,18 +21,24 @@ namespace BookShopping.Controllers
         {
             _context = context;
         }
-
+        public IActionResult Detail(int id)
+        {
+            List<Product> model = _context.Products.Include(x => x.Category).Where(x => x.CategoryId == id).ToList();
+            ViewBag.CategoryId = id;
+            return View(model);
+        }
 
         public IActionResult Index(int id)
         {
-            var model = _context.Products.Include(x => x.Category).Where(x => x.CategoryId == id).ToList();
+            List<Product> model = _context.Products.Include(x => x.Category).Where(x => x.CategoryId == id).ToList();
+            ViewBag.CategoryId = id;
             return View(model);
         }
         [HttpGet]
         public IActionResult Create(int id)
         {
-            var todo = new Product { CategoryId = id };
-            return View(todo);
+            var product = new Product { CategoryId = id };
+            return View(product);
         }
         [HttpPost]
         public IActionResult Create(Product shopping)
@@ -38,9 +47,7 @@ namespace BookShopping.Controllers
             {
                 if (ModelState.IsValid)
                 {
-
-                    var payment = _context.Categories.Find(shopping.CategoryId);
-                  
+                    var category = _context.Categories.Find(shopping.CategoryId);
                     _context.Products.Add(shopping);
                     _context.SaveChanges();
                     return RedirectToAction("Index", new { id = shopping.CategoryId });
@@ -48,36 +55,68 @@ namespace BookShopping.Controllers
             }
             catch (Exception)
             {
-
             }
             return View(shopping);
+
+
+            //if (shopping != null)
+            //{
+            //    string name = System.IO.Path.GetFileName(shopping.Picture);
+            //    string yol = "~/Images/" + name;
+
+            //}
+
+
+            // if (file != null)
+            //    {
+            //        string imageExtension = Path.GetExtension(file.Name);
+
+            //string imageName = Guid.NewGuid() + imageExtension;
+
+            //string path = Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot/images/{imageName}");
+
+            //        using var stream = new FileStream(path, FileMode.Create);
+
+
         }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var edit = _context.Products.FirstOrDefault(x => x.Id == id);
+            return View(edit);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product product, int id)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    var edit = _context.Products.FirstOrDefault(x=>x.Id==id);
+                    edit.Name = product.Name;
+                    edit.Price = product.Price;
+                    edit.Comment = product.Comment;
+                    _context.SaveChanges();
+                    return RedirectToAction("İndex", new { id = edit.CategoryId });
 
-        //[HttpGet]
-        //public IActionResult Create()
-        //{
-        //    return View();
-        //}
-        //[HttpPost]
-        //public IActionResult Create(Product model)
-        //{
-        //    try
-        //    {
-        //        if (ModelState.IsValid)
-        //        {
-        //            var product = _context.Products.Find(model.Id);
-        //            _context.Products.Add(product);
-        //            _context.SaveChanges();
-        //            return RedirectToAction();
-        //        }
-        //    }
-        //    catch (Exception)
-        //    {
+                }
+            }
+            catch (Exception)
+            {
 
-        //        throw;
-        //    }
-        //    return View(model);
-        
-
+                throw;
+            }
+            return View(product);
+        }
+        public IActionResult Delete(int id)
+        {
+            var delete = _context.Products.Find(id);
+            _context.Products.Remove(delete);
+            _context.SaveChanges();
+            return RedirectToAction("Index", new { id = delete.CategoryId });
+        }
     }
+
+
 }
+
