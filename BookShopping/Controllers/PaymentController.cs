b45db4dcb5f85032fff819bcc0c3326eeab536d6
eozkaya5 <eozkaya675@gmail.com>
2020-12-11 01:59:21 +0300

    Ödeme sayfa tasarımı oluşturuldu ve Controller 'da ödeme , listeleme methotları eklendi.

--- BookShopping/Controllers/PaymentController.cs
+++ BookShopping/Controllers/PaymentController.cs
@@ -1,16 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using BookShopping.Models.Context;
+using BookShopping.Models.ShoppingModel;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookShopping.Controllers
 {
     public class PaymentController : Controller
     {
-        public IActionResult Index()
+        readonly ShoppingDbContext _context;
+        public PaymentController(ShoppingDbContext context)
         {
-            return View();
+            _context = context;
         }
+        public IActionResult Index(int id,decimal totalPay)
+        {            
+            List<Payment> model = _context.Payments.Where(x => x.BasketId == id).ToList();
+            if (model != null )
+            {
+                totalPay = _context.Payments.Sum(x => x.TotalFee);
+                ViewBag.totalPay = +totalPay + "₺";
+            }           
+            ViewBag.BasketId = id;
+            return View(model);
+        }
+        [HttpGet]
+        public IActionResult Pay(int id)
+        {
+            var model = new Payment { BasketId = id };
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Pay(Payment model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {                
+                    var payment = _context.Baskets.Find(model.BasketId);
+                    payment.TotalFee -= model.TotalFee;
+                    model.Date = DateTime.Now;
+                    _context.Payments.Add(model);
+                    _context.SaveChanges();
+                    return RedirectToAction("Index","Basket", new { id = model.BasketId });
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            return View(model);
+        }
+
+
     }
+
 }
+
