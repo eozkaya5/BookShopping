798b23dc6390a6d82f91646ad9c0b05fda38d4a8
eozkaya5 <eozkaya675@gmail.com>
2020-12-19 23:41:25 +0300

    İletişim için  model oluşturuldu. Oluşturulan model code firts mantığıyla migration edildi.

---
+++ BookShopping/Models/ShoppingModel/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShopping.Models.ShoppingModel
+{
+    public class Contact
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string SurName { get; set; }
+        [Required]
+        public string EMail { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Message { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
