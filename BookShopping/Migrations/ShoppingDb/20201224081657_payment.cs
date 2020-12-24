984f29d7890c8eb6a4b10bf3a3a8124782e4d243
eozkaya5 <eozkaya675@gmail.com>
2020-12-24 11:34:49 +0300

    model

---
+++ BookShopping/Migrations/ShoppingDb/20201224081657_payment.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookShopping.Migrations.ShoppingDb
+{
+    public partial class payment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PaymentId",
+                table: "UserComments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PaymentId",
+                table: "Payments",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserComments_PaymentId",
+                table: "UserComments",
+                column: "PaymentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_PaymentId",
+                table: "Payments",
+                column: "PaymentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_Payments_PaymentId",
+                table: "Payments",
+                column: "PaymentId",
+                principalTable: "Payments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserComments_Payments_PaymentId",
+                table: "UserComments",
+                column: "PaymentId",
+                principalTable: "Payments",
+                principalColumn: "Id",
+                onUpdate: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_Payments_PaymentId",
+                table: "Payments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserComments_Payments_PaymentId",
+                table: "UserComments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserComments_PaymentId",
+                table: "UserComments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Payments_PaymentId",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentId",
+                table: "UserComments");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentId",
+                table: "Payments");
+        }
+    }
+}
