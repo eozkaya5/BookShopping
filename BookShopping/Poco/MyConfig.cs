98cd03c8af3513af660a12a0900f5349d1d5d60b
eozkaya5 <eozkaya675@gmail.com>
2021-01-02 00:19:45 +0300

    Poco sınıfı oluşturuldu.

--- BookShopping/Poco/MyConfig.cs
+++ BookShopping/Poco/MyConfig.cs
@@ -10,5 +10,7 @@ namespace BookShopping.Poco
         public string ConnectionString { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public int Port { get; set; }
+        public string Host { get; set; }
     }
 }
