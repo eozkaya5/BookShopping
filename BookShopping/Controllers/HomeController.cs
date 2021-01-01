69c229f54ea7c1cf79495ae302fff098ba9154b4
eozkaya5 <eozkaya675@gmail.com>
2021-01-02 00:16:01 +0300

    IConfigarition denemesi yapıldı.

--- BookShopping/Controllers/HomeController.cs
+++ BookShopping/Controllers/HomeController.cs
@@ -9,19 +9,22 @@ using BookShopping.Models;
 using Microsoft.AspNetCore.Authorization;
 using BookShopping.Models.ModelTypes;
 using BookShopping.Models.Context;
+using Microsoft.Extensions.Configuration;
 
 namespace BookShopping.Controllers
 {
-   
+
     public class HomeController : Controller
     {
         readonly ShoppingDbContext _context;
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration configuration;
 
-        public HomeController(ILogger<HomeController> logger, ShoppingDbContext context)
+        public HomeController(ILogger<HomeController> logger, ShoppingDbContext context, IConfiguration configuration)
         {
             _logger = logger;
             _context = context;
+            this.configuration = configuration;
         }
 
         public IActionResult Index()
@@ -30,6 +33,12 @@ namespace BookShopping.Controllers
             ViewModel model = new ViewModel();
             model.HomePage = _context.Products.ToList();
             model.SideBar = _context.Categories.ToList();
+
+            var email = configuration["MyConfig:Email"];
+            var password = configuration["MyConfig:Password"];
+
+
+
             return View(model);
         }
         public IActionResult Errors()
